e535e40084afad15ab538ca855b36c273f869a0e
msgalvan1207 <ms.galvan@uniandes.edu.co>
2024-05-28 14:09:27 -0500

    more tweaks

--- src/my_taller1/my_taller1/Servicio.py
+++ src/my_taller1/my_taller1/Servicio.py
@@ -162,8 +162,8 @@ class MinimalService(Node):
         error_y = self.posy_deseado - self.posy
         error_angular = math.atan2(-error_y,error_x) - self.orientation
         error_angular = math.atan2(math.sin(error_angular), math.cos(error_angular))
-        print(abs(error_angular))
-        if abs(error_angular) > math.radians(30):
+        print(abs(error_angular), math.radians(90))
+        if abs(error_angular) > math.radians(90):
             return True
         else:
             return False
